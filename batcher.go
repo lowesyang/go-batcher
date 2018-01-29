71b54513abdbc4a428764c16e2ce5b16306516a7
yyh1102 <234347589@qq.com>
2018-01-29 10:52:47 +0800

    fix bugs

--- batcher.go
+++ batcher.go
@@ -15,19 +15,19 @@ func NewBatcher() *Batcher {
 	}
 }
 
-func (bm *Batcher) AddBatcher(batch *Batch) {
+func (bm *Batcher) AddBatch(batch *Batch) {
 	bm.mutex.Lock()
 	bm.batches[batch.name] = batch
 	bm.mutex.Unlock()
 }
 
-func (bm *Batcher) GetBatcher(name string) *Batch {
+func (bm *Batcher) GetBatch(name string) *Batch {
 	bm.mutex.RLock()
 	defer bm.mutex.RUnlock()
 	return bm.batches[name]
 }
 
-func (bm *Batcher) DelBatcher(name string) {
+func (bm *Batcher) DelBatch(name string) {
 	bm.mutex.RLock()
 	delete(bm.batches, name)
 	bm.mutex.RUnlock()
